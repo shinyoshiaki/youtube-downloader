76007d7472dc73cf0ff79747a2427baf456dc77a
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-08-25 10:11:17 +0900

    status

--- pages/dl.tsx
+++ pages/dl.tsx
@@ -1,38 +1,50 @@
 import { NextPage } from "next";
 import { Button } from "@material-ui/core";
 import Link from "next/link";
-import { download } from "../src/domain/youtube";
+import { download, Progress } from "../src/domain/youtube";
 
 type Props = {
+  status: Progress;
   id?: string;
 };
 
-const DlPage: NextPage<Props> = ({ id }) => {
+const DlPage: NextPage<Props> = ({ id, status }) => {
+  const renderStatus = () => {
+    switch (status) {
+      case "completed":
+        return (
+          <a href={`/static/${id}.mp4`} download>
+            Click to download
+          </a>
+        );
+      case "downloading":
+        return <p>downloading</p>;
+      case "fail":
+        return <p>fail</p>;
+    }
+  };
+
   return (
     <div>
       <Link href={`/`}>
         <Button>Home</Button>
       </Link>
-      {id && (
-        <a href={`/static/${id}.mp4`} download>
-          Click to download
-        </a>
-      )}
+      {renderStatus()}
     </div>
   );
 };
 
 DlPage.getInitialProps = async ({ query }) => {
-  const { id } = query;
+  let { id } = query as any;
 
   if (id) {
     try {
-      const ready = await download(id as string);
-      return { query, ready, id: ready ? (id as string) : undefined };
+      const status = await download(id);
+      return { status, id };
     } catch (error) {
-      return {};
+      return { status: "fail" };
     }
-  } else return {};
+  } else return { status: "fail" };
 };
 
 export default DlPage;
--- src/domain/youtube.ts
+++ src/domain/youtube.ts
@@ -1,46 +1,52 @@
-export const download = async (youtubeId: string) => {
-  try {
-    const ytdl = await require("ytdl-core");
-    const fs = await import("fs");
-    const BASE_PATH = `https://www.youtube.com/watch?v=`;
+export const download = async (youtubeId: string): Promise<Progress> => {
+  const ytdl = await require("ytdl-core");
+  const fs = await import("fs");
+  const BASE_PATH = `https://www.youtube.com/watch?v=`;
 
-    const url = BASE_PATH + youtubeId;
+  const url = BASE_PATH + youtubeId;
 
-    const exist = await new Promise<boolean>(r =>
-      ytdl.getInfo(url, (err: any) => {
-        if (!err) {
-          r(true);
-        } else r(false);
-      })
-    );
+  const exist = await new Promise<boolean>(r =>
+    ytdl.getInfo(url, (err: any) => {
+      if (!err) {
+        r(true);
+      } else r(false);
+    })
+  );
 
-    if (!exist) return false;
+  if (!exist) return "fail";
 
-    return await new Promise<boolean>(async r => {
+  return await new Promise<Progress>(async r => {
+    try {
       const lock = `${youtubeId}.dl`;
       const file = `${youtubeId}.mp4`;
+
+      if (fs.existsSync(`static/${file}`)) {
+        r("completed");
+        return;
+      }
+
       if (!fs.existsSync(lock)) {
         fs.writeFileSync(lock, "");
         ytdl(url)
           .on("response", () => {
-            console.log("responce");
             fs.rename(file, `static/${file}`, () => {
-              console.log("ready");
               fs.unlinkSync(lock);
-              r(true);
+              r("completed");
             });
           })
           .on("data", () => {})
           .pipe(fs.createWriteStream(file));
 
         setTimeout(() => {
-          r(false);
+          r("downloading");
         }, 3000);
       } else {
-        r(true);
+        r("downloading");
       }
-    });
-  } catch (error) {
-    return false;
-  }
+    } catch (error) {
+      r("fail");
+    }
+  });
 };
+
+export type Progress = "downloading" | "completed" | "fail";
